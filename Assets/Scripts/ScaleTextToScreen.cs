1c8064ebefd9e3b867ab72aef9e61f15405947c3
CactusBento <cactusbento@outlook.com>
2023-10-31 14:51:54 -0400

    Start work on start menu

---
+++ Assets/Scripts/ScaleTextToScreen.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script is for the Start Menu to scale 
+/// the title text to the Start menu.
+/// 
+/// This is to facilitate the use of fonts to stylize
+/// the text of the game.
+/// </summary>
+public class ScaleTitle : MonoBehaviour
+{
+    RectTransform rt;
+
+    public float maxHeightRatio = 0.3f;
+    public float maxWidthRatio = 0.9f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rt = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rt.sizeDelta = new Vector2 (
+            maxWidthRatio * Screen.width,
+            maxHeightRatio * Screen.height
+        );
+    }
+}
